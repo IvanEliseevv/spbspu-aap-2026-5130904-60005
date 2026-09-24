78aec0d709f26b8ced66684555e95a134b193168
vladdd67 <vladkirillin079@gmail.com>
2026-09-25 00:59:32 +0300

    eto

--- kirillin.vladislav/P0/main.cpp
+++ kirillin.vladislav/P0/main.cpp
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-#include <iostream>
-
-int main()
-{
-    std::cout << "kirillin.vladislav\n";
-}
-=======
 #include <iostream>
 
 int main()
 {
   std::cout << "kirillin.vladislav\n";
 }
->>>>>>> 7921fda524a21f5cfa594311bba872864ea5d01a
